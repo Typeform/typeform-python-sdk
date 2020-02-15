f1be2bb37b8145a3b757ea09facdcfc8f83cd258
Pau Minoves <pau.minoves@typeform.com>
2020-02-15 22:52:28 +0100

    Mocking is not effective on all tests

--- typeform/test/fixtures.py
+++ typeform/test/fixtures.py
@@ -1,11 +1,15 @@
 import os
 
+# Set to True to use mocked requests.
+# A False here will make the test suite hit the real API. Set TOKEN and WORKSPACE below to make it work.
+MOCK = False
+
 # Don't use your own account for this, this will delete pretty much everything you have!
 TOKEN = os.environ.get('TYPEFORM_TOKEN')
 
 # Tests will be scoped to this workspace.
-WORKSPACE = 'https://api.typeform.com/workspaces/[YOUR WORKSPACE ID]'
+WORKSPACE = 'https://api.typeform.com/workspaces/hn3dNa'
 WORKSPACE_ID = WORKSPACE.split('/')[-1]
 
-if not TOKEN or '[YOUR WORKSPACE ID]' in WORKSPACE:
+if MOCK and (not TOKEN or '[YOUR WORKSPACE ID]' in WORKSPACE):
     raise Exception("You need to setup TOKEN and WORKSPACE in fixtures.py")
